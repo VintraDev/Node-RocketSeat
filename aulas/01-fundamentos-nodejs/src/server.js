9991002b6f35417638c89ebd716dfa319f19e048
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-04-22 13:24:34 -0300

    Feat - Métodos HTTP and URL

--- aulas/01-fundamentos-nodejs/src/server.js
+++ aulas/01-fundamentos-nodejs/src/server.js
@@ -1,9 +1,36 @@
 import http from 'node:http';
 
-// Criar um usuário (nome, email, senha)
+// HTTP
+// - Método HTTP
+// - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar uma informação do back-end
+// POST => Criar uma informação no back-end
+// PUT => Atualizar uma informação no back-end
+// PATCH => Atualizar uma informação esperífica de uma informação no back-end
+// DELETE => Deletar uma informação no back-end
+
+// GET /users => Buscando usuários do back-end
+// POST /users => Criando um usuário no back-end
+
+// GET - MÈTODO
+// /users - URL
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello World')
+
+    const { method, url } = req;
+
+    if (method === 'GET' && url === '/users') {
+        return res.end('Listagem');
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criação de usuários')
+    }
+
+    return res.end('Hello World!');
 })
 
 server.listen(3333)
